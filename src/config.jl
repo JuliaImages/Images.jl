d0602c20031c097aceccce91fa4a0116599d81d7
timholy <tim.holy@gmail.com>
2013-07-03 06:17:07 -0500

    Don't run which on Windows

--- src/config.jl
+++ src/config.jl
@@ -1,9 +1,11 @@
 ##########  Configuration   #########
 
 have_imagemagick = false
+@unix_only begin
 _, p = readsfrom(`which convert`)
 if !Base.wait_success(p)
     warn("ImageMagick utilities not found. Install for more file format support.")
 else
     have_imagemagick = true
+end
 end
