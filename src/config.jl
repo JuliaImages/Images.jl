f7ae0c7844bf77cd0e96723760939da7f2972395
Tim Holy <tim.holy@gmail.com>
2013-08-08 07:06:09 -0500

    Update for deletion of wait_success

--- src/config.jl
+++ src/config.jl
@@ -3,7 +3,8 @@
 have_imagemagick = false
 @unix_only begin
 _, p = readsfrom(`which convert`)
-if !Base.wait_success(p)
+wait(p)
+if !success(p)
     warn("ImageMagick utilities not found. Install for more file format support.")
 else
     have_imagemagick = true
