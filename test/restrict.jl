58e1398cc057184b3ef08258e108c01960d3d737
timholy <tim.holy@gmail.com>
2014-03-26 09:52:40 -0500

    Change url for huge image (test for IJulia/restrict)

--- test/restrict.jl
+++ test/restrict.jl
@@ -1,13 +1,16 @@
+# This is for testing display in IJulia. A huge image should
+# be automatically restricted before sending to IJulia for display.
+
 using Images
 
-url = "https://atmire.com/dspace-labs3/bitstream/handle/123456789/7618/earth-map-huge.jpg?sequence=1"
+url = "http://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg"
 
 const savedir = joinpath(tempdir(), "Images")
 if !isdir(savedir)
     mkdir(savedir)
 end
 
-const savename = joinpath(savedir, "earth-map-huge.jpg")
+const savename = joinpath(savedir, "The_Earth_seen_from_Apollo_17.jpg")
 if !isfile(savename)
     download(url, savename)
 end
