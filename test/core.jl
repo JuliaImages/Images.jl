74e1ec11fe9158ac5c66f6d4540f8f48c286e142
lucasb-eyer <lucasb.eyer.be@gmail.com>
2014-04-27 14:45:40 +0200

    s/@assert/@test/ in test/core.jl

--- test/core.jl
+++ test/core.jl
@@ -5,154 +5,154 @@ B = rand(1:20,3,5)
 cmap = uint8(repmat(linspace(12,255,20),1,3))
 img = ImageCmap(copy(B),cmap,["colorspace" => "RGB", "pixelspacing" => [2.0, 3.0], "spatialorder" => Images.yx])
 imgd = convert(Image, img)
-@assert eltype(img) == Uint8
-@assert eltype(imgd) == Uint8
+@test eltype(img) == Uint8
+@test eltype(imgd) == Uint8
 
 # basic information
-@assert size(img) == (3,5)
-@assert size(imgd) == (3,5,3)
-@assert ndims(img) == 2
-@assert ndims(imgd) == 3
+@test size(img) == (3,5)
+@test size(imgd) == (3,5,3)
+@test ndims(img) == 2
+@test ndims(imgd) == 3
 
 # copy/similar
 A = randn(3,5,3)
 imgc = copy(img)
-@assert imgc.data == img.data
+@test imgc.data == img.data
 imgc = copy(imgd, A)
-@assert imgc.data == A
+@test imgc.data == A
 img2 = similar(img)
-@assert isa(img2, ImageCmap)
-@assert img2.data != img.data
+@test isa(img2, ImageCmap)
+@test img2.data != img.data
 img2 = similar(imgd)
-@assert isa(img2, Image)
+@test isa(img2, Image)
 
 # ref/assign
 prev = img[4]
-@assert prev == B[4]
+@test prev == B[4]
 img[4] = prev+1
-@assert img.data[4] == prev+1
-@assert img[4] == prev+1
+@test img.data[4] == prev+1
+@test img[4] == prev+1
 
 # properties
-@assert colorspace(img) == "RGB"
-@assert colordim(img) == 0
-@assert colordim(imgd) == 3
-@assert timedim(img) == 0
-@assert limits(img) == (12,255)
-@assert limits(imgd) == (0,255)
-@assert pixelspacing(img) == [2.0, 3.0]
-@assert pixelspacing(imgd) == [2.0, 3.0]
+@test colorspace(img) == "RGB"
+@test colordim(img) == 0
+@test colordim(imgd) == 3
+@test timedim(img) == 0
+@test limits(img) == (12,255)
+@test limits(imgd) == (0,255)
+@test pixelspacing(img) == [2.0, 3.0]
+@test pixelspacing(imgd) == [2.0, 3.0]
 
-@assert sdims(img) == sdims(imgd)
-@assert coords_spatial(img) == coords_spatial(imgd)
-@assert size_spatial(img) == size_spatial(imgd)
+@test sdims(img) == sdims(imgd)
+@test coords_spatial(img) == coords_spatial(imgd)
+@test size_spatial(img) == size_spatial(imgd)
 
-@assert storageorder(img) == Images.yx
-@assert storageorder(imgd) == [Images.yx, "color"]
+@test storageorder(img) == Images.yx
+@test storageorder(imgd) == [Images.yx, "color"]
 
 A = rand(4,4,3)
-@assert colordim(A) == 3
+@test colordim(A) == 3
 Aimg = permutedims(convert(Image, A), [3,1,2])
-@assert colordim(Aimg) == 1
+@test colordim(Aimg) == 1
 
 # sub/slice
 s = sub(img, 2, 1:4)
-@assert ndims(s) == 2
-@assert sdims(s) == 2
-@assert size(s) == (1,4)
+@test ndims(s) == 2
+@test sdims(s) == 2
+@test size(s) == (1,4)
 s = subim(img, 2, 1:4)
-@assert ndims(s) == 2
-@assert sdims(s) == 2
-@assert size(s) == (1,4)
+@test ndims(s) == 2
+@test sdims(s) == 2
+@test size(s) == (1,4)
 s = sliceim(img, 2, 1:4)
-@assert ndims(s) == 1
-@assert sdims(s) == 1
-@assert size(s) == (4,)
+@test ndims(s) == 1
+@test sdims(s) == 1
+@test size(s) == (4,)
 s = sliceim(imgd, 2, 1:4, 1:3)
-@assert ndims(s) == 2
-@assert sdims(s) == 1
-@assert colordim(s) == 2
-@assert spatialorder(s) == ["x"]
+@test ndims(s) == 2
+@test sdims(s) == 1
+@test colordim(s) == 2
+@test spatialorder(s) == ["x"]
 s = sliceim(imgd, 2:2, 1:4, 1:3)
-@assert ndims(s) == 3
-@assert sdims(s) == 2
-@assert colordim(s) == 3
-@assert spatialorder(s) == ["y","x"]
+@test ndims(s) == 3
+@test sdims(s) == 2
+@test colordim(s) == 3
+@test spatialorder(s) == ["y","x"]
 
 # reslicing
 D = randn(3,5,4)
 sd = SliceData(D, 2)
 C = slice(D, sd, 2)
-@assert C == reshape(D[1:end, 2, 1:end], size(C))
+@test C == reshape(D[1:end, 2, 1:end], size(C))
 reslice!(C, sd, 3)
-@assert C == reshape(D[1:end, 3, 1:end], size(C))
+@test C == reshape(D[1:end, 3, 1:end], size(C))
 sd = SliceData(D, 3)
 C = slice(D, sd, 2)
-@assert C == reshape(D[1:end, 1:end, 2], size(C))
+@test C == reshape(D[1:end, 1:end, 2], size(C))
 
 sd = SliceData(imgd, 2)
 s = sliceim(imgd, sd, 2)
-@assert colordim(s) == 2
-@assert colorspace(s) == "RGB"
-@assert spatialorder(s) == ["y"]
-@assert s.data == reshape(imgd[:,2,:], size(s))
+@test colordim(s) == 2
+@test colorspace(s) == "RGB"
+@test spatialorder(s) == ["y"]
+@test s.data == reshape(imgd[:,2,:], size(s))
 sd = SliceData(imgd, 3)
 s = sliceim(imgd, sd, 2)
-@assert colordim(s) == 0
-@assert colorspace(s) == "Gray"
-@assert spatialorder(s) == Images.yx
-@assert s.data == imgd[:,:,2]
+@test colordim(s) == 0
+@test colorspace(s) == "Gray"
+@test spatialorder(s) == Images.yx
+@test s.data == imgd[:,:,2]
 reslice!(s, sd, 3)
-@assert s.data == imgd[:,:,3]
+@test s.data == imgd[:,:,3]
 
 # named indexing
-@assert dimindex(imgd, "color") == 3
-@assert dimindex(imgd, "y") == 1
-@assert dimindex(imgd, "z") == 0
+@test dimindex(imgd, "color") == 3
+@test dimindex(imgd, "y") == 1
+@test dimindex(imgd, "z") == 0
 imgdp = permutedims(imgd, [3,1,2])
-@assert dimindex(imgdp, "y") == 2
-@assert img["y", 2, "x", 4] == B[2,4]
-@assert img["x", 4, "y", 2] == B[2,4]
+@test dimindex(imgdp, "y") == 2
+@test img["y", 2, "x", 4] == B[2,4]
+@test img["x", 4, "y", 2] == B[2,4]
 chan = imgd["color", 2]
 Blookup = reshape(cmap[B[:],1], size(B))
-@assert chan == Blookup
+@test chan == Blookup
 
 sd = SliceData(imgd, "x")
 s = sliceim(imgd, sd, 2)
-@assert spatialorder(s) == ["y"]
-@assert s.data == reshape(imgd[:,2,:], size(s))
+@test spatialorder(s) == ["y"]
+@test s.data == reshape(imgd[:,2,:], size(s))
 sd = SliceData(imgd, "y")
 s = sliceim(imgd, sd, 2)
-@assert spatialorder(s) == ["x"]
-@assert s.data == reshape(imgd[2,:,:], size(s))
+@test spatialorder(s) == ["x"]
+@test s.data == reshape(imgd[2,:,:], size(s))
 sd = SliceData(imgd, "x", "y")
 s = sliceim(imgd, sd, 2, 1)
-@assert s.data == reshape(imgd[1,2,:], 3)
+@test s.data == reshape(imgd[1,2,:], 3)
 
 # spatial order, width/height, and permutations
-@assert spatialpermutation(Images.yx, imgd) == [1,2]
-@assert widthheight(imgd) == (5,3)
+@test spatialpermutation(Images.yx, imgd) == [1,2]
+@test widthheight(imgd) == (5,3)
 C = convert(Array, imgd)
-@assert C == imgd.data
+@test C == imgd.data
 imgd.properties["spatialorder"] = ["x", "y"]
-@assert spatialpermutation(Images.xy, imgd) == [1,2]
-@assert widthheight(imgd) == (3,5)
+@test spatialpermutation(Images.xy, imgd) == [1,2]
+@test widthheight(imgd) == (3,5)
 C = convert(Array, imgd)
-@assert C == permutedims(imgd.data, [2,1,3])
+@test C == permutedims(imgd.data, [2,1,3])
 imgd.properties["spatialorder"] = ["y", "x"]
-@assert spatialpermutation(Images.xy, imgd) == [2,1]
+@test spatialpermutation(Images.xy, imgd) == [2,1]
 imgd.properties["spatialorder"] = ["x", "L"]
-@assert spatialpermutation(Images.xy, imgd) == [1,2]
+@test spatialpermutation(Images.xy, imgd) == [1,2]
 imgd.properties["spatialorder"] = ["L", "x"]
-@assert spatialpermutation(Images.xy, imgd) == [2,1]
-@assert spatialpermutation(Images.xy, A) == [2,1]
-@assert spatialpermutation(Images.yx, A) == [1,2]
+@test spatialpermutation(Images.xy, imgd) == [2,1]
+@test spatialpermutation(Images.xy, A) == [2,1]
+@test spatialpermutation(Images.yx, A) == [1,2]
 
 imgd.properties["spatialorder"] = Images.yx
 imgp = permutedims(imgd, ["x", "y", "color"])
-@assert imgp.data == permutedims(imgd.data, [2,1,3])
+@test imgp.data == permutedims(imgd.data, [2,1,3])
 imgp = permutedims(imgd, ("color", "x", "y"))
-@assert imgp.data == permutedims(imgd.data, [3,2,1])
+@test imgp.data == permutedims(imgd.data, [3,2,1])
 
 ## Convenience constructors
 # colorim
