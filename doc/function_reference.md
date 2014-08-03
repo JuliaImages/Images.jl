c8b7575184240993266cb0f6b0c6548c6082e251
Kevin Squire <kevin@secondspectrum.com>
2014-08-03 16:28:57 -0700

    Edge thinning updates
    * Discretize image gradients on the fly, to avoid temporary allocation
      of a discretized gradient image, per Tim Holy's suggestion
    * Add versions of thin_edges_nonmaxsup!, thin_edges_nonmaxsup_subpix!
      which allow results to be placed in preallocated arrays

--- doc/function_reference.md
+++ doc/function_reference.md
@@ -709,7 +709,7 @@ Edge thinning for 2D edge images.  Currently the only algorithm available is non
 
 `border` is any of the boundary conditions specified in `padarray`.
 
-In addition to the maximal edge image, the `_subpix` versions of these functions also return an estimate of the subpixel location of each local maxima, as a 2D array or image of Base.Graphics.Point objects.  Additionally, each local maxima is adjusted to the estimated value at the subpixel location.
+In addition to the maximal edge image, the `_subpix` versions of these functions also return an estimate of the subpixel location of each local maxima, as a 2D array or image of `Base.Graphics.Point` objects.  Additionally, each local maxima is adjusted to the estimated value at the subpixel location.
 
 Currently, the `_nonmaxsup` functions are identical to the first two function calls, except that they also accept additional keyword arguments.  `radius` indicates the step size to use when searching in the direction of the gradient; values between 1.2 and 1.5 are suggested (default 1.35).  `theta` indicates the step size to use when discretizing angles in the `gradientangle` image, in radians (default: 1 degree in radians = pi/180).
 
@@ -720,6 +720,14 @@ Example:
     mag, grad_angle = magnitude_phase(gx,gy)
     mag[mag .< 0.5] = 0.0  # Threshold magnitude image    
     thinned, subpix =  thin_edges_subpix(mag, gradient)
+
+<br />
+```
+thin_edges_nonmaxsup!(out, img, gradientangle, [border]; [radius::Float64=1.35], [theta=pi/180])
+thin_edges_nonmaxsup_subpix!(out, location, img, gradientangle, [border]; [radius::Float64=1.35], [theta=pi/180])
+```
+
+For advanced usage, these versions will put results into preallocated arrays or images.  `out` must be the same size and type as `img`.  `location` must be an array of type `Graphics.Point` and must be the same size as `img`.
 
 <br />
 ```
--- src/nonmaxsup.jl
+++ src/nonmaxsup.jl
@@ -3,15 +3,14 @@
 # Usage:
 #          (im,location) = nonmaxsup(img, gradientangles, radius);
 #
-# Function for performing non-maxima suppression on an image using an
-# orientation image.  It is assumed that the orientation image gives 
-# gradient angles in radians.
+# Function for performing non-maxima suppression on an image using
+# gradient angles.  Gradient angles are assumed to be in radians.
 #
 # Input:
 #   img - image to be non-maxima suppressed.
 # 
-#   gradientangles  - image containing gradient angles around each pixel in radians
-#             (-pi,pi)
+#   gradientangles - image containing gradient angles around each pixel in radians
+#                    (-pi,pi)
 # 
 #   radius  - Distance in pixel units to be looked at on each side of each
 #             pixel when determining whether it is a local maxima or not.
@@ -20,11 +19,8 @@
 #
 # Returns:
 #   im        - Non maximally suppressed image.
-#   location  - Complex valued image holding subpixel locations of edge
-#               points. For any pixel the real part holds the subpixel row
-#               coordinate of that edge point and the imaginary part holds
-#               the column coordinate.  (If a pixel value is 0+0i then it
-#               is not an edgepoint.)
+#   location  - `Graphics.Point` image holding subpixel locations of edge
+#               points. 
 #
 # Notes:
 #
@@ -50,7 +46,7 @@
 # August    2005 - Made Octave compatible
 # October   2013 - Final thinning applied to binary image for Octave
 #                  compatbility (Thanks to Chris Pudney)
-# June      2014 - Converted (and changed significantly) to Julia (Kevin Squire)
+# June      2014 - Converted (and modified significantly) to Julia (Kevin Squire)
 
 import Base.Graphics.Point
 
@@ -93,20 +89,11 @@ function _calc_discrete_offsets(θ, radius, transposed)
         yoffs = [CoordOffset(-y) for y in  radius*sin(angles)]
     end
 
-    return xoffs, yoffs
+    return θ, xoffs, yoffs
 end
 
-# Discretize an array of orientation angles
-function _discretize_angles(gradientangles::AbstractArray, θ=pi/180)
-    iθ = 1/θ
-    d_gradientangles = similar(gradientangles, Int)
-    for i = 1:length(d_gradientangles)
-        g = gradientangles[i]
-        angle = g < 0 ? (g+2π)*iθ : g*iθ
-        d_gradientangles[i] = iround(angle)+1
-    end
-    d_gradientangles
-end
+_discretize_angle(angle::FloatingPoint, invθ) = 
+    angle < 0 ? iround((angle + 2π)*invθ)+1 : iround(angle*invθ)+1
 
 # Interpolate the value of an offset from a particular pixel
 #
@@ -137,24 +124,20 @@ end
 
 cv2_count = 0
 
-# Core thinning algorithm using nonmaximal suppression
-function thin_edges_nonmaxsup_core!{T}(location, img::AbstractArray{T,2}, gradientangles, radius, border, theta)
+# Core edge thinning algorithm using nonmaximal suppression
+function thin_edges_nonmaxsup_core!{T}(out::AbstractArray{T,2}, location::AbstractArray{Point,2}, 
+                                       img::AbstractArray{T,2}, gradientangles, radius, border, theta)
     calc_subpixel = !isempty(location)
 
     # Error checking
-    size(img) == size(gradientangles) || error("image and gradient orientation image are of different sizes")
+    size(img) == size(gradientangles) == size(out) || error("image, gradient angle, and output image must all be the same size")
+    calc_subpixel && size(location) != size(img) && error("subpixel location has a different size than the input image")
     radius < 1.0 && error("radius must be >= 1")
 
     # Precalculate x and y offsets relative to centre pixel for each orientation angle 
     transposed = spatialorder(img)[1] == "x"
-    xoffs, yoffs = _calc_discrete_offsets(theta, radius, transposed)
-
-    # Discretize orientation image
-    d_gradientangles = _discretize_angles(gradientangles, theta)
-
-    # Allocate output
-    (height,width) = size(img)
-    out = zeros(T, height, width)
+    θ, xoffs, yoffs = _calc_discrete_offsets(theta, radius, transposed)
+    iθ = 1/θ
 
     # Indexes to use for border handling
     pad = iceil(radius)
@@ -164,10 +147,12 @@ function thin_edges_nonmaxsup_core!{T}(location, img::AbstractArray{T,2}, gradie
     # Now run through the image interpolating grey values on each side
     # of the centre pixel to be used for the non-maximal suppression.
 
+    (height,width) = size(img)
+
     for x = 1:width, y = 1:height
         (c = img[y,x]) == 0 && continue  # For thresholded images
 
-        or = d_gradientangles[y,x]   # Disretized orient
+        or = _discretize_angle(gradientangles[y,x],iθ)   # Disretized orientation
         v1, n1 = _interp_offset(img, x, y, xoffs[or], yoffs[or], Ix, Iy, pad)
 
         if (c > v1) & (c >= n1) # We need to check the value on the other side...
@@ -205,18 +190,39 @@ function thin_edges_nonmaxsup_core!{T}(location, img::AbstractArray{T,2}, gradie
     out
 end
 
-# Main function call when subpixel location of edges is not needed
-thin_edges_nonmaxsup(img::AbstractArray, gradientangles::AbstractArray, border::String="replicate";
-                     radius::Float64=1.35, theta=pi/180) =
-    copy(img, thin_edges_nonmaxsup_core!(Array(Point,(0,0)), img, gradientangles, radius, border, theta))
 
+# Main function call when subpixel location of edges is not needed
+function thin_edges_nonmaxsup!{A<:AbstractArray,B<:AbstractArray}(out::A, img::A, gradientangles::B, border::String="replicate";
+                                                                  radius::Float64=1.35, theta=pi/180)
+    properties(out) != properties(img) && error("Input and output arrays must have the same properties")
+    thin_edges_nonmaxsup_core!(data(out), Array(Point,(0,0)), img, gradientangles, radius, border, theta)
+    out
+end
+
+function thin_edges_nonmaxsup{T}(img::AbstractArray{T,2}, gradientangles::AbstractArray, border::String="replicate";
+                                 radius::Float64=1.35, theta=pi/180)
+    (height,width) = size(img)
+    out = zeros(T, height, width)
+    thin_edges_nonmaxsup_core!(out, Array(Point,(0,0)), img, gradientangles, radius, border, theta)
+    copy(img, out)
+end
 
 # Main function call when subpixel location of edges is desired
-function thin_edges_nonmaxsup_subpix(img::AbstractArray, gradientangles::AbstractArray,
+function thin_edges_nonmaxsup_subpix!{A<:AbstractArray, B<:AbstractArray, C<:AbstractArray}(out::A, location::B, img::A, gradientangles::C,
                                      border::String="replicate"; radius::Float64=1.35, theta=pi/180)
+    properties(out) != properties(img) && error("Input and output arrays must have the same properties")
+    eltype(location) != Point && error("Preallocated subpixel location array/image must have element type Graphics.Point")
+
+    thin_edges_nonmaxsup_core!(data(out), data(location), img, gradientangles, radius, border, theta)
+    img, location
+end
+
+function thin_edges_nonmaxsup_subpix{T}(img::AbstractArray{T}, gradientangles::AbstractArray,
+                                        border::String="replicate"; radius::Float64=1.35, theta=pi/180)
     (height,width) = size(img)
+    out = zeros(T, height, width)
     location = zeros(Point, height, width)
-    out = thin_edges_nonmaxsup_core!(location, img, gradientangles, radius, border, theta)
+    thin_edges_nonmaxsup_core!(out, location, img, gradientangles, radius, border, theta)
 
     copy(img, out), copy(img, location)
 end
