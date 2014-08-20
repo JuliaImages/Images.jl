80a647d293606aa46960814f87ac9606bc9ab322
timholy <tim.holy@gmail.com>
2014-08-20 04:18:58 -0500

    Change OSXnative reader to use ColorValues

--- src/colortypes.jl
+++ src/colortypes.jl
@@ -34,6 +34,30 @@ BGR(r::Fractional, g::Fractional, b::Fractional) = (T = promote_type(typeof(r), 
 typealias BGRA{T} AlphaColorValue{BGR{T}, T}
 
 
+# Some readers return a byte for an alpha channel even if it's not meaningful
+immutable RGB1{T<:Fractional} <: AbstractRGB{T}
+    alphadummy::T
+    r::T
+    g::T
+    b::T
+
+    RGB1(r::Number, g::Number, b::Number) = new(one(T), r, g, b)
+end
+RGB1(r::Integer, g::Integer, b::Integer) = RGB1{Float64}(r, g, b)
+RGB1(r::Fractional, g::Fractional, b::Fractional) = (T = promote_type(typeof(r), typeof(g), typeof(b)); RGB1{T}(r, g, b))
+
+immutable RGB4{T<:Fractional} <: AbstractRGB{T}
+    r::T
+    g::T
+    b::T
+    alphadummy::T
+
+    RGB4(r::Number, g::Number, b::Number) = new(r, g, b, one(T))
+end
+RGB4(r::Integer, g::Integer, b::Integer) = RGB4{Float64}(r, g, b)
+RGB4(r::Fractional, g::Fractional, b::Fractional) = (T = promote_type(typeof(r), typeof(g), typeof(b)); RGB4{T}(r, g, b))
+
+
 # For encoding GrayAlpha
 immutable Gray{T<:Fractional} <: ColorValue{T}
     val::T
