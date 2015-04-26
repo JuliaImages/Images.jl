JuliaImages/Images.jl

commit f203853c5b8dafc2115a1a7ba29a31ceeda18892

Add a very simple imresize

src/algorithms.jl
@@ -991,6 +991,37 @@ end
 
 restrict_size(len::Integer) = isodd(len) ? (len+1)>>1 : (len>>1)+1
 
+## imresize
+function imresize!(resized, original)
+    assert2d(original)
+    scale1 = (size(original,1)-1)/(size(resized,1)-0.999f0)
+    scale2 = (size(original,2)-1)/(size(resized,2)-0.999f0)
+    for jr = 0:size(resized,2)-1
+        jo = scale2*jr
+        ijo = itrunc(jo)
+        fjo = jo - oftype(jo, ijo)
+        @inbounds for ir = 0:size(resized,1)-1
+            io = scale1*ir
+            iio = itrunc(io)
+            fio = io - oftype(io, iio)
+            tmp = (1-fio)*((1-fjo)*original[iio+1,ijo+1] +
+                           fjo*original[iio+1,ijo+2]) +
+                  + fio*((1-fjo)*original[iio+2,ijo+1] +
+                         fjo*original[iio+2,ijo+2])
+            resized[ir+1,jr+1] = convertsafely(eltype(resized), tmp)
+        end
+    end
+    resized
+end
+
+imresize(original, new_size) = imresize_julia!(similar(original, new_size), original)
+
+convertsafely{T<:FloatingPoint}(::Type{T}, val) = convert(T, val)
+convertsafely{T<:Integer}(::Type{T}, val::Integer) = convert(T, val)
+convertsafely{T<:Integer}(::Type{T}, val::FloatingPoint) = itrunc(T, val+oftype(val, 0.5))
+convertsafely{T}(::Type{T}, val) = convert(T, val)
+
+
 function imlineardiffusion{T}(img::Array{T,2}, dt::FloatingPoint, iterations::Integer)
     u = img
     f = imlaplacian()

commit b4c9a8652cd5a2b2675eec50de813f3d4bae3060

Fixed ERROR: imresize_julia! not defined
Call to imresize() resulted in ERROR: imresize_julia! not defined
Bug introduced in f203853c5b8dafc2115a1a7ba29a31ceeda18892
Initially function imresize was named imresize_julia (see https://groups.google.com/forum/#!msg/julia-users/wG1g9C7S6Mg/zx9Pqntv1e0J) to compare different imresize approaches

src/algorithms.jl
@@ -1037,7 +1037,7 @@ function imresize!(resized, original)
     resized
 end
 
-imresize(original, new_size) = imresize_julia!(similar(original, new_size), original)
+imresize(original, new_size) = imresize!(similar(original, new_size), original)
 
 convertsafely{T<:FloatingPoint}(::Type{T}, val) = convert(T, val)
 convertsafely{T<:Integer}(::Type{T}, val::Integer) = convert(T, val)

test/algorithms.jl
@@ -322,3 +322,8 @@ P = [ 0.0  0.0  0.0   0.0   0.0   0.0   0.0  0.0;
 
 Q = Images.shepp_logan(8,highContrast=false)
 @test norm((P-Q)[:]) < 1e-10
+
+# image resize
+img = convert(Images.Image, zeros(10,10))
+img2 = Images.imresize(img, (5,5))
+@test length(img2) == 25