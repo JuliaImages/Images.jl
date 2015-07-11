86e3644828d012efd6f3809b864e701d4fc5efa1
Tim Holy <tim.holy@gmail.com>
2015-07-11 10:15:07 -0500

    Fix parallel test

--- test/parallel.jl
+++ test/parallel.jl
@@ -1,13 +1,20 @@
 addprocs(2)
-using Images, Color
+# Normally "using Images" would suffice, but since Images has
+# already been loaded by the time this file runs, that yields
+# a no-op. So we have to force the workers to load it manually.
+# See https://github.com/JuliaLang/julia/issues/3674
+@sync for p in workers()
+    @spawnat p Base.require("Images")
+end
+using Color
 
 # Issue #287
 @everywhere function test287(img)
     return 0;
 end
-Imgs = Array(Image{RGB{Float64}}, 2);
-Imgs[1] = convert(Image{RGB}, rand(100, 100, 3));
-Imgs[2] = convert(Image{RGB}, rand(100, 100, 3));
+Imgs = Array(Images.Image{RGB{Float64}}, 2);
+Imgs[1] = convert(Images.Image{RGB}, rand(100, 100, 3));
+Imgs[2] = convert(Images.Image{RGB}, rand(100, 100, 3));
 
 let Imgs = Imgs;
     ret = pmap(i -> test287(Imgs[i]), 1:2);
