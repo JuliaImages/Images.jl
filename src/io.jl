52e82ee9440b69539f32818ee887c8fcdd677b5d
Tim Holy <tim.holy@gmail.com>
2015-08-16 08:08:29 -0500

    Small cleanup for indexed images
    This improves type stability but probably has no functional consequence

--- src/io.jl
+++ src/io.jl
@@ -204,12 +204,11 @@ mimewritable(::MIME"image/png", img::AbstractImage) = sdims(img) == 2 && timedim
 # See IJulia issue #229
 mimewritable{T<:ColorValue}(::MIME"image/svg+xml", ::AbstractMatrix{T}) = false
 
+writemime(stream::IO, ::MIME"image/png", img::AbstractImageIndexed; kwargs...) =
+    writemime(stream, MIME"image/png", convert(Image, img); kwargs...)
+
 function writemime(stream::IO, ::MIME"image/png", img::AbstractImage; mapi=mapinfo_writemime(img), minpixels=10^4, maxpixels=10^6)
     assert2d(img)
-    if isa(img, AbstractImageIndexed)
-        # For now, convert to direct
-        img = convert(Image, img)
-    end
     A = data(img)
     nc = ncolorelem(img)
     npix = length(A)/nc
@@ -328,16 +327,14 @@ end
 
 imread{C<:ColorType}(filename::String, ::Type{ImageMagick}, ::Type{C}) = convert(Image{C}, imread(filename, ImageMagick))
 
+imwrite(img::AbstractImageIndexed, filename::String, ::Type{ImageMagick}; kwargs...) = imwrite(convert(Image, img), filename, ImageMagick; kwargs...)
+
 function imwrite(img, filename::String, ::Type{ImageMagick}; mapi = mapinfo(ImageMagick, img), quality = nothing)
     wand = image2wand(img, mapi, quality)
     LibMagick.writeimage(wand, filename)
 end
 
 function image2wand(img, mapi, quality)
-    if isa(img, AbstractImageIndexed)
-        # For now, convert to direct
-        img = convert(Image, img)
-    end
     imgw = map(mapi, img)
     imgw = permutedims_horizontal(imgw)
     have_color = colordim(imgw)!=0
