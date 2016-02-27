3fc4eeae1d475e22d0fffae9b82436b124eaf6f2
Tim Holy <tim.holy@gmail.com>
2016-02-27 13:55:27 -0600

    Test writemime/restrict using a small array
    This should make debugging easier

--- test/writemime.jl
+++ test/writemime.jl
@@ -22,6 +22,15 @@ facts("Writemime") do
         @fact data(b) --> A[[1,1,2,2],[1,1,2,2]]
     end
     context("big images (use of restrict)") do
+        A = U8[0.01 0.4 0.99; 0.25 0.8 0.75; 0.6 0.2 0.0]
+        Ar = restrict(A)
+        fn = joinpath(workdir, "writemime.png")
+        open(fn, "w") do file
+            writemime(file, MIME("image/png"), grayim(A), minpixels=0, maxpixels=5)
+        end
+        b = load(fn)
+        @fact data(b) --> convert(Array{U8}, Ar)
+        # a genuinely big image (tests the defaults)
         abig = grayim(rand(UInt8, 1024, 1023))
         fn = joinpath(workdir, "big.png")
         open(fn, "w") do file
