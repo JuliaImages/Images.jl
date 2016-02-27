0ea2844ff24a9c64e510038f131e573ded2dd11a
Tim Holy <tim.holy@gmail.com>
2016-02-27 08:32:49 -0600

    Rework writemime to use save(stream, img)

--- test/writemime.jl
+++ test/writemime.jl
@@ -1,19 +1,43 @@
-writemime_(io::IO, ::MIME"image/png", img::AbstractImage) = serialize(io, map(Images.mapinfo_writemime(img), img))
 facts("Writemime") do
-	workdir = joinpath(tempdir(), "Images")
+    workdir = joinpath(tempdir(), "Images")
     if !isdir(workdir)
         mkdir(workdir)
     end
-    context("use of restrict") do
+    context("no compression or expansion") do
+        A = U8[0.01 0.99; 0.25 0.75]
+        fn = joinpath(workdir, "writemime.png")
+        open(fn, "w") do file
+            writemime(file, MIME("image/png"), grayim(A), minpixels=0, maxpixels=typemax(Int))
+        end
+        b = load(fn)
+        @fact data(b) --> A
+    end
+    context("small images (expansion)") do
+        A = U8[0.01 0.99; 0.25 0.75]
+        fn = joinpath(workdir, "writemime.png")
+        open(fn, "w") do file
+            writemime(file, MIME("image/png"), grayim(A), minpixels=5, maxpixels=typemax(Int))
+        end
+        b = load(fn)
+        @fact data(b) --> A[[1,1,2,2],[1,1,2,2]]
+    end
+    context("big images (use of restrict)") do
+        A = U8[0.01 0.4 0.99; 0.25 0.8 0.75; 0.6 0.2 0.0]
+        Ar = restrict(A)
+        fn = joinpath(workdir, "writemime.png")
+        open(fn, "w") do file
+            writemime(file, MIME("image/png"), grayim(A), minpixels=0, maxpixels=5)
+        end
+        b = load(fn)
+        @fact data(b) --> convert(Array{U8}, Ar)
+        # a genuinely big image (tests the defaults)
         abig = grayim(rand(UInt8, 1024, 1023))
         fn = joinpath(workdir, "big.png")
         open(fn, "w") do file
             writemime(file, MIME("image/png"), abig, maxpixels=10^6)
         end
-        b = open(fn, "r") do io 
-        	deserialize(io)
-        end
+        b = load(fn)
         abigui = convert(Array{UFixed8,2}, data(restrict(abig, (1,2))))
-        @fact data(b) --> convert(Array{UFixed8,2}, data(restrict(abig, (1,2))))
+        @fact data(b) --> abigui
     end
 end
