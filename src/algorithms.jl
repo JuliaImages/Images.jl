1efe986bcf0f9ab2874780a4c8e123815399c6ae
lucasb-eyer <lucasb.eyer.be@gmail.com>
2016-03-16 11:32:40 +0100

    Minor doc fix for `imstretch`.

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1342,7 +1342,7 @@ end
 `imgs = imstretch(img, m, slope)` enhances or reduces (for
 slope > 1 or < 1, respectively) the contrast near saturation (0 and 1). This is
 essentially a symmetric gamma-correction. For a pixel of brightness `p`, the new
-intensity is `1/(1+m/(p+eps)^slope)`.
+intensity is `1/(1+(m/(p+eps))^slope)`.
 
 This assumes the input `img` has intensities between 0 and 1.
 """
