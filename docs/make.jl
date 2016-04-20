a0e4efb3404631b14358140fb375a9c6ee7bab10
Ron Rock <rrock@uchicago.edu>
2016-04-20 17:25:54 -0500

    Fixup Documenter theme

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter, Images
 
 makedocs(doctest=false)
 
-deploydocs(deps=Deps.pip("pygments", "mkdocs", "mk"),
+deploydocs(deps=Deps.pip("pygments", "mkdocs", "mkdocs-material"),
            repo="github.com/timholy/Images.jl.git",
            julia="0.4",
            osname="linux")
