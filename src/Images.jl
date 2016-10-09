ad505c97a6034877b781569f3ff749337f18b5c0
Tim Holy <tim.holy@gmail.com>
2016-10-09 05:46:59 -0500

    Reimplement overlays with StackedView

--- src/Images.jl
+++ src/Images.jl
@@ -42,7 +42,7 @@ typealias RealLike                           Union{Real,AbstractGray}
 import Graphics
 import Graphics: width, height, Point
 using StatsBase  # TODO: eliminate this dependency
-using IndirectArrays
+using IndirectArrays, MappedArrays
 
 const is_little_endian = ENDIAN_BOM == 0x04030201
 
@@ -56,7 +56,7 @@ using ImageMetadata: ImageMetaAxis
 using Base.Cartesian  # TODO: delete this
 
 include("map-deprecated.jl")
-include("overlays.jl")
+include("overlays-deprecated.jl")
 include("labeledarrays.jl")
 include("algorithms.jl")
 include("exposure.jl")
--- test/old/overlays.jl
+++ test/old/overlays.jl
@@ -5,65 +5,66 @@ facts("Overlay") do
     gray = linspace(0.0, 1.0, 5)
     context("One") do
         ovr = Images.Overlay((2gray, 2gray), (RGB(1, 0, 0), RGB(0, 0, 1)), (Clamp{Float64}(), Clamp{Float64}()))
-        @fact ovr[1] --> RGB(0, 0, 0)
-        @fact ovr[2] --> RGB{U8}(0.5, 0, 0.5)
-        @fact ovr[3] --> ovr[4]
-        @fact ovr[4] --> ovr[5]
-        @fact ovr[5] --> exactly(RGB(1, 0, 1))
-        @fact eltype(ovr) --> RGB{U8}
-        @fact length(ovr) --> 5
-        @fact size(ovr) --> (5,)
-        @fact size(ovr, 1) --> 5
-        @fact size(ovr, 2) --> 1
-        @fact nchannels(ovr) --> 2
+        @fact ovr[1] --> RGB(0, 0, 0) "test XcqmPT"
+        @fact ovr[2] --> RGB{U8}(0.5, 0, 0.5) "test yAFSVQ"
+        @fact ovr[3] --> ovr[4] "test dyKCV9"
+        @fact ovr[4] --> ovr[5] "test d4aUI1"
+        @fact ovr[5] --> exactly(RGB(1, 0, 1)) "test UF1jSj"
+        @fact eltype(ovr) --> RGB{U8} "test Es9OnV"
+        @fact length(ovr) --> 5 "test 04ptpL"
+        @fact size(ovr) --> (5,) "test iE5wc4"
+        @fact size(ovr, 1) --> 5 "test OvtQ4m"
+        @fact size(ovr, 2) --> 1 "test O1oIKi"
+        @fact nchannels(ovr) --> 2 "test vhNvBL"
         @fact raw(ovr) --> [0x00 0x80 0xff 0xff 0xff;
                             0x00 0x00 0x00 0x00 0x00;
-                            0x00 0x80 0xff 0xff 0xff]
+                            0x00 0x80 0xff 0xff 0xff] "test vLlExB"
         @fact separate(ovr) --> UFixed8[0   0   0;
                                         0.5 0   0.5;
                                         1   0   1;
                                         1   0   1;
-                                        1   0   1]
+                                        1   0   1] "test XrmTTp"
         iob = IOBuffer()
         show(iob, ovr)  # exercise only
     end
 
     context("Two") do
         ovr = Images.Overlay((gray, 0*gray), (RGB{UFixed8}(1, 0, 1), RGB{UFixed8}(0, 1, 0)), ((0, 1), (0, 1)))
-        @fact eltype(ovr) --> RGB{UFixed8}
+        @fact eltype(ovr) --> RGB{UFixed8} "test u7gavU"
+        ovr = collect(ovr)
         s = similar(ovr)
-        @fact typeof(s) --> Vector{RGB{UFixed8}}
-        @fact length(s) --> 5
+        @fact typeof(s) --> Vector{RGB{UFixed8}} "test qOV0Iu"
+        @fact length(s) --> 5 "test HxDDe4"
         s = similar(ovr, RGB{Float32})
-        @fact isa(s, Vector{RGB{Float32}}) --> true
-        @fact length(s) --> 5
+        @fact isa(s, Vector{RGB{Float32}}) --> true "test tPX9bh"
+        @fact length(s) --> 5 "test CEJl6T"
         s = similar(ovr, RGB{Float32}, (3, 2))
-        @fact isa(s, Matrix{RGB{Float32}}) --> true
-        @fact size(s) --> (3, 2)
+        @fact isa(s, Matrix{RGB{Float32}}) --> true "test qQvSYP"
+        @fact size(s) --> (3, 2) "test uoSePw"
         buf = Images.uint32color(ovr)
-        gray8 = [round(UInt8, 255 * x) for x in gray]
-        nogreen = reinterpret(RGB24, [convert(UInt32, g)<<16 | convert(UInt32, g) for g in gray8])
-        @fact buf --> nogreen
+        gray8 = round(UInt8, 255*gray)
+        nogreen = [convert(UInt32, g)<<16 | convert(UInt32, g) for g in gray8]
+        @fact buf --> nogreen "test XOvI8c"
     end
 
     context("Three") do
         ovr = Images.Overlay((gray, 0*gray), [RGB(1, 0, 1), RGB(0, 1, 0)], ([0, 1], [0, 1]))
-        @fact_throws ErrorException Images.Overlay((gray, 0*gray), (RGB(1, 0, 1), RGB(0, 1, 0)), ((0,), (0, 1)))
-        @fact_throws ErrorException ovr[1] = RGB(0.2, 0.4, 0.6)
+        @fact_throws ErrorException Images.Overlay((gray, 0*gray), (RGB(1, 0, 1), RGB(0, 1, 0)), ((0,), (0, 1))) "test AKzBSc"
+        @fact_throws ErrorException ovr[1] = RGB(0.2, 0.4, 0.6) "test uxe7aR"
     end
 
     context("Four") do
         img1 = Images.Image(gray)
         ovr = Images.OverlayImage((2gray, img1), (RGB{Float32}(1, 0, 1), RGB{Float32}(0, 1, 0)), ((0, 1),(0, 1)))
-        @fact isa(ovr, Images.Image) --> true
-        @fact haskey(ovr, "colorspace") --> false
-        @fact Images.colorspace(ovr) --> "RGB"
-        @fact ovr[2] --> RGB{Float32}(0.5, 0.25, 0.5)
+        @fact isa(ovr, Images.Image) --> true "test AcmCRK"
+        @fact haskey(ovr, "colorspace") --> false "test W1xBNq"
+        @fact Images.colorspace(ovr) --> "RGB" "test me6TWH"
+        @fact ovr[2] --> RGB{Float32}(0.5, 0.25, 0.5) "test lznPxT"
         a = rand(Float32, 3, 2)
         b = rand(Float32, 3, 2)
         ovr = Images.OverlayImage((a, b), (RGB{Float32}(1, 0, 1), RGB{Float32}(0, 1, 0)), ((0, 1), (0, 1)))
-        @fact isa(ovr, Images.Image) --> true
-        @fact abs(ovr[1, 2] - RGB{Float32}(a[1, 2], b[1, 2], a[1, 2])) --> roughly(0, atol=1e-5)
+        @fact isa(ovr, Images.Image) --> true "test 8nGPCi"
+        @fact abs(ovr[1, 2] - RGB{Float32}(a[1, 2], b[1, 2], a[1, 2])) --> roughly(0, atol=1e-5) "test 5N74oJ"
     end
 
     context("permutation") do
