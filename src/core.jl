ae993a747b3cffc0e4bbf4d8dea88c08f7230393
Frank Preiswerk <frank@bwh.harvard.edu>
2016-10-10 13:56:49 -0400

    Support for imresize on color images

--- src/core.jl
+++ src/core.jl
@@ -1183,16 +1183,6 @@ function assert_colordim_third(img::AbstractArray)
 end
 
 """
-`assert_same_size(img1,img2)` triggers an error if both images are not of the
-same size.
-"""
-function assert_same_size(img1::AbstractArray,img2::AbstractArray)
-  if size(img1) != size(img2)
-    error("Images are not of same size")
-  end
-end
-
-"""
 `tf = isyfirst(img)` tests whether the first spatial dimension is `"y"`.
 
 See also: `isxfirst`, `assert_yfirst`.
