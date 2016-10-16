e914b5e6ecd7954714399b531a97d611c45f4018
Tim Holy <tim.holy@gmail.com>
2016-10-16 07:57:33 -0500

    Send 8-bit colors to Jupyter

--- src/showmime.jl
+++ src/showmime.jl
@@ -16,7 +16,7 @@ mimewritable{C<:Color}(::MIME"image/svg+xml", img::AbstractMatrix{C}) = false
 # Really large images can make display very slow, so we shrink big
 # images.  Conversely, tiny images don't show up well, so in such
 # cases we repeat pixels.
-function Base.show{C<:Colorant}(io::IO, mime::MIME"image/png", img::AbstractMatrix{C}; mapi=clamp01nan, minpixels=10^4, maxpixels=10^6)
+function Base.show{C<:Colorant}(io::IO, mime::MIME"image/png", img::AbstractMatrix{C}; mapi=x->map8(clamp01nan(x)), minpixels=10^4, maxpixels=10^6)
     while _length(img) > maxpixels
         img = restrict(img)  # big images
     end
@@ -30,3 +30,7 @@ function Base.show{C<:Colorant}(io::IO, mime::MIME"image/png", img::AbstractMatr
     end
     save(Stream(format"PNG", io), img, mapi=mapi)
 end
+
+# Jupyter seemingly can't handle 16-bit colors
+map8(c::Colorant) = mapc(UFixed8, c)
+map8(x::Number) = UFixed8(x)
