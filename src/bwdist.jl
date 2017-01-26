aea16ef52b662180cf7cb5996faf8c1dec7daccc
Daniel Muehlbachler <daniel.muehlbachler@catalysts.cc>
2017-01-26 08:58:59 +0100

    adapt changes to support rectangular inputs

--- src/bwdist.jl
+++ src/bwdist.jl
@@ -2,12 +2,12 @@ using Base.Cartesian
 
 
 """
-    permutesubs!(subs, perm::Vector{Int}, result:AbstractArray{Int})
+    permutesubs!(subs::Tuple, perm::Vector{Int}, result:AbstractArray{Int})
 
 Permute a tuple of subscripts given a permutation vector and
 writing the permutation into `result`.
 """
-function permutesubs!(subs, perm::Vector{Int}, result::AbstractArray{Int})
+function permutesubs!(subs::Tuple, perm::Vector{Int}, result::AbstractArray{Int})
   n = length(subs)
   @inbounds @simd for i = 1:n
     result[i] = subs[perm[i]]
@@ -16,20 +16,21 @@ function permutesubs!(subs, perm::Vector{Int}, result::AbstractArray{Int})
 end
 
 """
-    permutedimsubs!(F::AbstractArray{Int, N}, perm::AbstractVector{Int}, stride::AbstractArray{Int}, sizeI::Tuple, B::AbstractArray{Int, N}, tempArray::AbstractArray{Int})
+    permutedimsubs!(F::AbstractArray{Int, N}, perm::AbstractVector{Int}, tempArray::AbstractArray{Int})
 
 Permute the dimensions of an array and those of the linear indices stored in that array,
-using `tempArray` as a temporary array for permuting the subscripts,
-`B` as a temporary array for permuted dims, and `stride` as the strides of array `F`.
+using `tempArray` as a temporary array for permuting the subscripts.
 """
-function permutedimsubs!{N}(F::AbstractArray{Int, N}, perm::Vector{Int}, stride::AbstractArray{Int}, sizeI::Tuple, B::AbstractArray{Int, N}, tempArray::AbstractArray{Int})
-  permutedims!(B, F, perm)
+function permutedimsubs!{N}(F::AbstractArray{Int, N}, perm::Vector{Int}, tempArray::AbstractArray{Int})
+  sizeF = size(F)
+  B = permutedims(F, perm)
 
-  @inbounds @simd for i = 1:length(B)
-    F[i] = B[i] == 0 ? 0 : stridedSub2Ind(stride, permutesubs!(ind2sub(sizeI, B[i]), perm, tempArray))
+  stride = collect(strides(B))
+  @inbounds for i = 1:length(B)
+    B[i] = B[i] == 0 ? 0 : stridedSub2Ind(stride, permutesubs!(ind2sub(sizeF, B[i]), perm, tempArray))
   end
 
-  return F
+  return B
 end
 
 """
@@ -42,7 +43,7 @@ Replacing `sub2ind` in order to reduce memory consumption.
 function stridedSub2Ind(stride::AbstractArray{Int}, i::AbstractArray{Int})
   s = 1
   @inbounds @fastmath for j = 1:length(stride)
-    s += (i[j]-1)*stride[j]
+    s += (i[j] - 1)*stride[j]
   end
   return s
 end
@@ -71,9 +72,11 @@ euclidean(a::AbstractVector, b::AbstractVector) = sqrt(sumsqdiff(a, b))
 
 
 """
-    bwdist(I::AbstractArray{Bool, N})
+    bwdist(I::AbstractArray{Bool, N}) -> F, D
 
-Compute the euclidean distance and feature transform of a binary image.
+Compute the euclidean distance and feature transform of a binary image,
+where `F` is trhe feature transform (the closest feature pixel map)
+and `D` is the euclidean distance transform of `I`.
 
 Implemented according to
 'A Linear Time Algorithm for Computing
@@ -90,18 +93,17 @@ Binary Images in Arbitrary Dimensions' [Maurer et al., 2003]
     # generate strides array
     stride = collect(strides(I))
 
-    # F and D, we use D as a temporary array for permutedimsubs
+    # F and D
     F = zeros(Int, sizeI)
     D = zeros(Int, sizeI)
 
     _computeft!(F, I, stride)
     d = 1:$N
     @inbounds for i = d
-      _voronoift!(F, I, stride, sizeI, tempArray)
-      F = permutedimsubs!(F, circshift(d, 1), stride, sizeI, D, tempArray)
+      _voronoift!(F, I, tempArray)
+      F = permutedimsubs!(F, circshift(d, 1), tempArray)
     end
 
-    setindex!(D, 0)
     @inbounds @nloops $N i F begin
       x = 1
       (@nexprs $N j -> (x += (i_j - 1)*stride[j]))
@@ -115,7 +117,9 @@ end
 """
     \_computeft!(F::AbstractArray{Int, N}, I::AbstractArray{Bool, N}, stride::AbstractArray{Int})
 
-Compute F_0 in the parlance of Maurer et al. 2003.
+Compute F_0, indicating the closest feature voxel in the 0th dimension where
+for each voxel x in I `F_0(x) = x if x = 1 and 0 otherwise`,
+in the parlance of Maurer et al. 2003.
 """
 @generated function _computeft!{N}(F::AbstractArray{Int, N}, I::AbstractArray{Bool, N}, stride::AbstractArray{Int})
   quote
@@ -127,27 +131,28 @@ Compute F_0 in the parlance of Maurer et al. 2003.
 end
 
 """
-    \_voronoift!(F::AbstractArray{Int, N}, I::AbstractArray{Bool, N}, stride::AbstractArray{Int}, sizeI::Tuple, g::AbstractArray{Int})
+    \_voronoift!(F::AbstractArray{Int, N}, I::AbstractArray{Bool, N}, g::AbstractArray{Int})
 
 Compute the partial Voronoi diagram along the first dimension of F,
 using g as a temporary array, following Maurer et al. 2003.
 """
-@generated function _voronoift!{N}(F::AbstractArray{Int, N}, I::AbstractArray{Bool, N}, stride::AbstractArray{Int}, sizeI::Tuple, g::AbstractArray{Int})
+@generated function _voronoift!{N}(F::AbstractArray{Int, N}, I::AbstractArray{Bool, N}, g::AbstractArray{Int})
   quote
-    @inbounds @nloops $N d j -> (j == 1 ? 0 : 1:sizeI[j]) begin
-      # FIXME - this needs nearly all memory allocations!
-      #Fstar = (@nref $N F j -> (j == 1 ? (:) : d_j))
+    sizeF = size(F)
+    stride = collect(strides(F))
+
+    @inbounds @nloops $N d j -> (j == 1 ? 0 : 1:sizeF[j]) begin
       l = 0
       setindex!(g, 0)
 
-      @inbounds for i = 1:sizeI[1]
-        f = (@nref $N F j -> (j == 1 ? i : d_j)) #Fstar[i]
+      @inbounds for i = 1:sizeF[1]
+        f = (@nref $N F j -> (j == 1 ? i : d_j))
         if f != 0
           if l < 2
             l += 1
             g[l] = f
           else
-            while l >= 2 && removeft(g[l-1], g[l], f, d_2, sizeI)
+            while l >= 2 && removeft(g[l-1], g[l], f, d_2, sizeF)
               l -= 1
             end
             l += 1
@@ -160,11 +165,11 @@ using g as a temporary array, following Maurer et al. 2003.
       if n_s == 0
       else
         l = 1
-        @inbounds for d_1 = 1:sizeI[1]
+        @inbounds for d_1 = 1:sizeF[1]
           # This makes a vector x containing the appropriate subscripts
           x = 1
           (@nexprs $N j -> (x += (d_j - 1)*stride[j]))
-          while l < n_s && (euclidean(x, g[l], sizeI) > euclidean(x, g[l+1], sizeI))
+          while l < n_s && (euclidean(x, g[l], sizeF) > euclidean(x, g[l+1], sizeF))
             l += 1
           end
           (@nref $N F d) = g[l]
@@ -175,11 +180,11 @@ using g as a temporary array, following Maurer et al. 2003.
 end
 
 """
-    removeft(u::Int, v::Int, w::INt, r::Int, dims)
+    removeft(u::Int, v::Int, w::INt, r::Int, dims::Tuple)
 
 Calculate whether we should remove a feature pixel from the Voronoi diagram.
 """
-function removeft(u::Int, v::Int, w::Int, r::Int, dims)
+function removeft(u::Int, v::Int, w::Int, r::Int, dims::Tuple)
   u1 = ind2sub(dims, u)[1]
   v1 = ind2sub(dims, v)[1]
   w1 = ind2sub(dims, w)[1]
@@ -191,21 +196,21 @@ function removeft(u::Int, v::Int, w::Int, r::Int, dims)
 end
 
 """
-    distance2(u::Int, r::Int, dims)
+    distance2(u::Int, r::Int, dims::Tuple)
 
 Calculate the Squared Euclidean distance of u, given by a linear index, to a row index.
 """
-function distance2(u::Int, r::Int, dims)
+function distance2(u::Int, r::Int, dims::Tuple)
   u2 = ind2sub(dims, u)[2]
   return (u2 - r)^2
 end
 
 """
-    sqeuclidean(x::Int, g::Int, dims)
+    sqeuclidean(x::Int, g::Int, dims::Tuple)
 
 Calculate the Squared Euclidean Distance between linear indices.
 """
-function sqeuclidean(x::Int, g::Int, dims)
+function sqeuclidean(x::Int, g::Int, dims::Tuple)
   x_subs = ind2sub(dims, x)
   g_subs = ind2sub(dims, g)
 
@@ -217,4 +222,4 @@ function sqeuclidean(x::Int, g::Int, dims)
 end
 
 # Euclidean distance between linear indices
-euclidean(x::Int, g::Int, dims) = sqrt(sqeuclidean(x, g, dims))
+euclidean(x::Int, g::Int, dims::Tuple) = sqrt(sqeuclidean(x, g, dims))
