db327d955359d91ca374db25192ba91ab1823f79
David Anthoff <anthoff@berkeley.edu>
2017-06-13 15:36:17 -0400

    Wrap tests in one master testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,7 @@
+using Base.Test
+
+@testset "Images" begin
+
 include("arrays.jl")
 include("algorithms.jl")
 include("exposure.jl")
@@ -6,5 +10,8 @@ include("corner.jl")
 include("bwdist.jl")
 include("distances.jl")
 include("writemime.jl")
+
+end
+
 info("\n\nBeginning of tests with deprecation warnings\n\n")
 include("old/runtests.jl")
