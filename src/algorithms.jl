2b2673a3a2b858c9d6f29c3ee89839a6a0ada034
tejus-gupta <tejusgupta14@gmail.com>
2017-06-18 19:51:13 +0530

    works for n dimension

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -922,7 +922,7 @@ Parameters:
 -    bins        = Number of bins used to compute the histogram. Needed for floating-point images.    
     
 """
-function otsu_threshold{T<:Union{Gray,Real}}(img::AbstractArray{T, 2}, bins::Int = 256)
+function otsu_threshold{T<:Union{Gray,Real}, N}(img::AbstractArray{T, N}, bins::Int = 256)
 
     min, max = extrema(img)
     edges, counts = imhist(img, linspace(gray(min), gray(max), bins))
@@ -966,22 +966,23 @@ Parameters:
 -    sigma       = standard deviation for kernel if "gaussian" method is used
     
 """
-function adaptive_threshold{T<:Union{Gray,Real}}(img::AbstractArray{T, 2}, block_size::Int, offset::Real, method = "mean"; sigma::Float64 = (block_size - 1) / 6.0)
+function adaptive_threshold{T<:Union{Gray,Real}, N}(img::AbstractArray{T, N}, block_size::Int, offset::Real, method = "mean"; sigma::Float64 = (block_size - 1) / 6.0)
 
     block_size>0 || error("block_size must greater than zero and odd\n")
     isodd(block_size) || error("block_size must be odd\n")
     
     if method == "mean"
-        kernel = centered(fill(1/block_size^2, (block_size, block_size)))
+        kernel1 = fill(1/block_size, block_size)
+        kernel = kernelfactors(map(centered, ntuple(d->kernel1, ndims(img))))
         thres = imfilter(img, kernel)
     elseif method == "median"
-        thres = mapwindow(median!, map(x->gray(x), img), (block_size, block_size))
+        thres = mapwindow(median!, map(x->gray(x), img), ntuple(x->block_size, ndims(img)))
     elseif method == "gaussian"
-        kernel = Kernel.gaussian((sigma, sigma), (block_size, block_size))
+        kernel = Kernel.gaussian(ntuple(x->sigma, ndims(img)), ntuple(x->block_size, ndims(img)))
         thres = imfilter(img, kernel)
     end
     
-    return convert(Array{T, 2}, thres - offset)
+    return convert(Array{T, N}, thres - offset)
 end
 
 """
@@ -1001,7 +1002,7 @@ Parameters:
     4. truncate_inverted: f(x, thres) = x if x > thres else thres
     
 """
-function threshold{T<:Union{Gray,Real}}(img::AbstractArray{T, 2}, thres::T, method = "standard")
+function threshold{T<:Union{Gray,Real}, N}(img::AbstractArray{T, N}, thres::T, method = "standard")
 
     if method == "standard"
         return map(x -> x>thres ? one(T) : zero(T), img)
@@ -1015,7 +1016,7 @@ function threshold{T<:Union{Gray,Real}}(img::AbstractArray{T, 2}, thres::T, meth
 
 end
 
-function threshold{T<:Gray}(img::AbstractArray{T, 2}, thres::AbstractArray{T, 2}, method = "standard")
+function threshold{T<:Gray, N}(img::AbstractArray{T, N}, thres::AbstractArray{T, N}, method = "standard")
 
     if method == "standard"
         return map( (x,y) -> x>y ? one(T) : zero(T), img, thres)
