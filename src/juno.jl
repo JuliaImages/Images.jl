dc0afdfa9f4740964ef18d4187a966f24f4931a2
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2017-07-12 12:27:12 +0200

    juno plot pane display

---
+++ src/juno.jl
@@ -0,0 +1,16 @@
+using Requires
+
+@require Juno begin
+    import Media
+    if Juno.isactive()
+        Juno.media(Images.ColorantMatrix, Media.Graphical)
+
+        function Juno.render(e::Juno.Editor, img::Images.ColorantMatrix)
+            Juno.render(e, nothing)
+        end
+
+        function Juno.render(pane::Juno.PlotPane, img::Images.ColorantMatrix)
+            Juno.render(pane, HTML("<img src=\"data:image/png;base64,$(stringmime(MIME("image/png"), img))\">"))
+        end
+    end
+end
