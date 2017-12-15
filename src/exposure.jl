0eb845a7e40d36ec7caedf9c02b8c22ef24b9124
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-12-15 14:58:43 +0100

    Update exposure.jl
    Added a comment to `complement`'s docstring

--- src/exposure.jl
+++ src/exposure.jl
@@ -26,7 +26,8 @@ imstretch(img::ImageMeta, m::Number, slope::Number) = shareproperties(img, imstr
     y = complement(x)
 
 Take the complement `1-x` of `x`.  If `x` is a color with an alpha channel,
-the alpha channel is left untouched.
+the alpha channel is left untouched. Don't forget to add a dot when `x` is
+an array: `complement.(x)`
 """
 complement(x) = oneunit(x)-x
 complement(x::TransparentColor) = typeof(x)(complement(color(x)), alpha(x))
