def00ed44a83f4612c5ba955bdae50a31d3cbd01
arijitkar98 <arijit.kar14@gmail.com>
2018-04-17 08:10:38 +0530

    Adds tests for boundaries function

--- test/algorithms.jl
+++ test/algorithms.jl
@@ -661,7 +661,88 @@ using Base.Test
                      0 4 5 4 0
                      0 0 0 0 0]
         @test cleared_img == check_img
-    end    
+    end
+
+    @testset "boundaries" begin
+        #Method = 'Thick' Case
+        img = zeros(Int, 10, 10)
+        img[3:7,3:7] = 1
+        check_img = [0 0 0 0 0 0 0 0 0 0
+                     0 1 1 1 1 1 1 1 0 0
+                     0 1 1 1 1 1 1 1 0 0
+                     0 1 1 0 0 0 1 1 0 0
+                     0 1 1 0 0 0 1 1 0 0
+                     0 1 1 0 0 0 1 1 0 0
+                     0 1 1 1 1 1 1 1 0 0
+                     0 1 1 1 1 1 1 1 0 0
+                     0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0]
+        res_img = boundaries(img,1)
+        @test res_img == check_img
+
+        #Method = 'Inner' Case
+        img = zeros(Int, 10, 10)
+        img[3:7,3:7] = 1
+        check_img = [0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0
+                     0 0 1 1 1 1 1 0 0 0
+                     0 0 1 0 0 0 1 0 0 0
+                     0 0 1 0 0 0 1 0 0 0
+                     0 0 1 0 0 0 1 0 0 0
+                     0 0 1 1 1 1 1 0 0 0
+                     0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0]
+        res_img = boundaries(img,2)
+        @test res_img == check_img
+
+        #Method = 'Outer' Case
+        img = zeros(Int, 10, 10)
+        img[3:7,3:7] = 1
+        check_img = [0 0 0 0 0 0 0 0 0 0
+                     0 1 1 1 1 1 1 1 0 0
+                     0 1 0 0 0 0 0 1 0 0
+                     0 1 0 0 0 0 0 1 0 0
+                     0 1 0 0 0 0 0 1 0 0
+                     0 1 0 0 0 0 0 1 0 0
+                     0 1 0 0 0 0 0 1 0 0
+                     0 1 1 1 1 1 1 1 0 0
+                     0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0]
+        res_img = boundaries(img,3)
+        @test res_img == check_img
+
+        #Method = 'Subpixel' Case
+        img = zeros(Int, 5, 4)
+        img[3,2] = 1
+        img[2:4,3] = 2
+        check_img = [0 0 0 0 0 0 0
+                     0 0 0 1 1 1 0
+                     0 0 0 1 0 1 0
+                     0 1 1 1 0 1 0
+                     0 1 0 1 0 1 0
+                     0 1 1 1 0 1 0
+                     0 0 0 1 0 1 0
+                     0 0 0 1 1 1 0
+                     0 0 0 0 0 0 0]
+        res_img = boundaries(img,4)
+        @test res_img == check_img
+
+        #Boolean input Case
+        img = falses(9,9)
+        img[3:5,3:5] = true
+        check_img = [false false false false false false false false false
+                     false true true true true true false false false
+                     false true true true true true false false false
+                     false true true false true true false false false
+                     false true true true true true false false false
+                     false true true true true true false false false
+                     false false false false false false false false false
+                     false false false false false false false false false
+                     false false false false false false false false false]
+        res_img = boundaries(img,1)
+        @test res_img == check_img
+    end
 end
 
 nothing
