4792579ffb9fc3f3dda832f51346b384b362a8a5
Simon Byrne <simonbyrne@gmail.com>
2018-06-18 13:40:59 -0700

    doc is Julia not TeX

--- src/edge.jl
+++ src/edge.jl
@@ -51,7 +51,7 @@ m = magnitude(grad_x, grad_y)
 ```
 
 Calculates the magnitude of the gradient images given by `grad_x` and `grad_y`.
-Equivalent to ``sqrt(grad_x.^2 + grad_y.^2)``.
+Equivalent to `sqrt(grad_x.^2 + grad_y.^2)`.
 
 Returns a magnitude image the same size as `grad_x` and `grad_y`.
 """
