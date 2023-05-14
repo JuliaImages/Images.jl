d9e358e23d7aeddbec2f829c8f56a0e8ea253b3e
ashwani rathee <ab669522@gmail.com>
2023-05-14 17:00:18 +0530

    corner tests for testing exported functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,7 @@ include("arrays.jl")
 include("algorithms.jl")
 @suppress_err include("exposure.jl") # deprecated
 include("edge.jl")
+include("corner.jl")
 include("writemime.jl")
 
 @suppress_err include("legacy.jl")
