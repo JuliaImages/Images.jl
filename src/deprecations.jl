ef6acdcfd9f4fefb4dd2aa884a43016322ed4715
Tim Holy <tim.holy@gmail.com>
2023-07-23 14:56:59 -0500

    Add release procedure in CONTRIBUTING (#1039)

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,2 +1,6 @@
+## v0.26 deprecations
+
 @deprecate otsu_threshold(img::AbstractArray{T}, nbins::Int = 256) where {T<:Union{Gray,Real}} T(find_threshold(img, Otsu(); nbins))
 @deprecate yen_threshold(img::AbstractArray{T}, nbins::Int = 256) where {T<:Union{Gray,Real}} T(find_threshold(img, Yen(); nbins))
+
+## v0.27 deprecations
